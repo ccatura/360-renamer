1f85a74ab92aca2ce3ba1faece6cec720630bd1d
ccatura <charlie@charliecatura.com>
2023-02-17 14:51:24 -0500

    added drag-drop folder to rename all image frames

--- 360-renamer.bat
+++ 360-renamer.bat
@@ -1,2 +1,28 @@
 @echo off
 
+:: RENAMES ALL IMAGES TO PROPER CONVENTION FOR 360 FOLDER
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+set /a count=1
+
+for %%a in (%1\*) do (
+    set thepath=%%~pa
+    set thename=%%~na
+    set theext=%%~xa
+    set origname=C:!thepath!!thename!!theext!
+
+    if !count! LSS 10 (
+        set renamed=img0!count!.jpg
+    ) else (
+        set renamed=img!count!.jpg
+    )
+
+    echo Original Name:      !origname!
+    echo    Renamed To:      !renamed!
+    rename "!origname!" "!renamed!"
+    echo .
+
+    set /a count+=1
+)
+ENDLOCAL
+
+
